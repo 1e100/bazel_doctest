ef81787f5046dc1fde8a280c1ee42bd191fa3c56
1e100 <38598618+1e100@users.noreply.github.com>
2020-09-09 03:04:01 -0700

    Add initial code

---
+++ module.py
@@ -0,0 +1,12 @@
+""" This is a test module. """
+
+def a_plus_b(a: int, b: int) -> int:
+    """ Adds `a` and `b`.
+    >>> a_plus_b(0, 3)
+    3
+    >>> a_plus_b(-3, 3)
+    0
+    >>> a_plus_b(2, 2)
+    4
+    """
+    return a + b
